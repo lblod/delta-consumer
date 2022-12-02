34be2c6e94eb174d0a6c3823f27eb0a29a9daf92
Boris De Vloed <boris.de.vloed@gmail.com>
2022-12-02 14:46:40 +0100

    Provide fetch as part of lib

--- pipelines/delta-sync.js
+++ pipelines/delta-sync.js
@@ -2,8 +2,8 @@ import * as muAuthSudo from '@lblod/mu-auth-sudo';
 import * as mu from 'mu';
 import fetcher from '../lib/fetcher';
 import {
-    DELTA_SYNC_JOB_OPERATION, DISABLE_DELTA_INGEST, INITIAL_SYNC_JOB_OPERATION,
-    JOBS_GRAPH, JOB_CREATOR_URI, SERVICE_NAME, SYNC_FILES_ENDPOINT, WAIT_FOR_INITIAL_SYNC
+  DELTA_SYNC_JOB_OPERATION, DISABLE_DELTA_INGEST, INITIAL_SYNC_JOB_OPERATION,
+  JOBS_GRAPH, JOB_CREATOR_URI, SERVICE_NAME, SYNC_FILES_ENDPOINT, WAIT_FOR_INITIAL_SYNC
 } from '../config';
 import { STATUS_BUSY, STATUS_FAILED, STATUS_SUCCESS } from '../lib/constants';
 import DeltaFile from '../lib/delta-file';
@@ -13,6 +13,7 @@ import { createError, createJobError } from '../lib/error';
 import { createJob, failJob, getJobs, getLatestJobForOperation } from '../lib/job';
 import { updateStatus } from '../lib/utils';
 import { deltaSyncDispatching } from '../triples-dispatching';
+import * as fetch from 'node-fetch';
 
 export async function startDeltaSync() {
   try {
@@ -24,7 +25,7 @@ export async function startDeltaSync() {
       console.log(`Status of WAIT_FOR_INITIAL_SYNC is: ${WAIT_FOR_INITIAL_SYNC}`);
       let previousInitialSyncJob;
 
-      if (WAIT_FOR_INITIAL_SYNC){
+      if (WAIT_FOR_INITIAL_SYNC) {
         previousInitialSyncJob = await getLatestJobForOperation(INITIAL_SYNC_JOB_OPERATION, JOB_CREATOR_URI);
       }
 
@@ -36,9 +37,9 @@ export async function startDeltaSync() {
         //Note: it is ok to fail these, because we assume it is running in a queue. So there is no way
         // a job in status busy was effectively doing something
         console.log(`Verify whether there are hanging jobs`);
-        const jobs = await getJobs(DELTA_SYNC_JOB_OPERATION, [ STATUS_BUSY ]);
+        const jobs = await getJobs(DELTA_SYNC_JOB_OPERATION, [STATUS_BUSY]);
         console.log(`Found ${jobs.length} hanging jobs, failing them first`);
-        for(const job of jobs){
+        for (const job of jobs) {
           await failJob(job.job);
         }
 
@@ -47,7 +48,7 @@ export async function startDeltaSync() {
 
     }
   }
-  catch(e) {
+  catch (e) {
     console.log(e);
     await createError(JOBS_GRAPH, SERVICE_NAME, `Unexpected error while running normal sync task: ${e}`);
   }
@@ -60,21 +61,21 @@ async function runDeltaSync() {
     const latestDeltaTimestamp = await calculateLatestDeltaTimestamp();
     const sortedDeltafiles = await getSortedUnconsumedFiles(latestDeltaTimestamp);
 
-    if(sortedDeltafiles.length) {
+    if (sortedDeltafiles.length) {
       job = await createJob(JOBS_GRAPH, DELTA_SYNC_JOB_OPERATION, JOB_CREATOR_URI, STATUS_BUSY);
 
       let parentTask;
-      for(const [ index, deltaFile ] of sortedDeltafiles.entries()) {
+      for (const [index, deltaFile] of sortedDeltafiles.entries()) {
         console.log(`Ingesting deltafile created on ${deltaFile.created}`);
         const task = await createDeltaSyncTask(JOBS_GRAPH, job, `${index}`, STATUS_BUSY, deltaFile, parentTask);
         try {
           const termObjectChangeSets = await deltaFile.load();
-          await deltaSyncDispatching.dispatch({ mu, muAuthSudo }, { termObjectChangeSets });
+          await deltaSyncDispatching.dispatch({ mu, muAuthSudo, fetch }, { termObjectChangeSets });
           await updateStatus(task, STATUS_SUCCESS);
           parentTask = task;
           console.log(`Sucessfully ingested deltafile created on ${deltaFile.created}`);
         }
-        catch(e){
+        catch (e) {
           console.error(`Something went wrong while ingesting deltafile created on ${deltaFile.created}`);
           console.error(e);
           await updateStatus(task, STATUS_FAILED);
@@ -89,7 +90,7 @@ async function runDeltaSync() {
     }
   }
   catch (error) {
-    if(job){
+    if (job) {
       await createJobError(JOBS_GRAPH, job, error);
       await failJob(job);
     }
