4f24ae25e462254fe1a279777296c8883e04243e
Nordine Bittich <contact@bittich.be>
2025-10-23 15:17:07 +0200

    use a more realistic example

--- triples-dispatching/example-custom-dispatching/delta-sync-dispatching.js
+++ triples-dispatching/example-custom-dispatching/delta-sync-dispatching.js
@@ -1,31 +1,22 @@
-/**
- * Dispatch the fetched information to a target graph.
- * @param { mu, muAuthSudo, fetch } lib - The provided libraries from the host service.
- * @param { termObjectChangeSets: { deletes, inserts } } data - The fetched changes sets, which objects of serialized Terms
- *          [ {
- *              graph: "<http://foo>",
- *              subject: "<http://bar>",
- *              predicate: "<http://baz>",
- *              object: "<http://boom>^^<http://datatype>"
- *            }
- *         ]
- * @return {void} Nothing
- */
-export async function dispatch(lib, data){
-  const { mu, muAuthSudo } = lib;
-  const { termObjectChangeSets } =  data;
+/* Variables */
+const INGEST_GRAPH =
+    process.env.INGEST_GRAPH || `http://mu.semte.ch/graphs/public`;
 
-  console.log(`Found an amount of ${termObjectChangeSets.length} changesets`);
-  for (let { deletes, inserts } of termObjectChangeSets) {
-    console.log(`Logging delete information: `);
-    const deleteStatements = deletes.map(o => `In graph: ${o.graph}, triple: ${o.subject} ${o.predicate} ${o.object}`);
-    deleteStatements.forEach(s => console.log(s));
+const sparqlEndpoint = "http://database:8890/sparql";
 
-    console.log(`Logging insert information: `);
-    const insertStatements = inserts.map(o => `In graph: ${o.graph}, triple: ${o.subject} ${o.predicate} ${o.object}.`);
-    insertStatements.forEach(s => console.log(s));
-  }
-  console.log(`All changeSets were logged, waiting for next update!`);
+/* Codes */
+export async function dispatch(lib, data) {
+    const { insertIntoGraph, deleteFromGraph } = lib;
+    const { termObjectChangeSets } = data;
+
+    for (let { deletes, inserts } of termObjectChangeSets) {
+
+        console.log(`Using ${sparqlEndpoint} to insert triples`);
+
+        await deleteFromGraph(deletes, sparqlEndpoint, INGEST_GRAPH, {});
+        await insertIntoGraph(inserts, sparqlEndpoint, INGEST_GRAPH, {});
+
+
+    }
 }
-
 
